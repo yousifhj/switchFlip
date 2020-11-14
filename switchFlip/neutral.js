9a4194bbe8edf76d0477ed54f58fd9852f0f01e4
Huda Yousif <hjoski@gmail.com>
2020-11-14 11:19:00 -0800

    create neutral flipper

--- switchFlip/neutral.js
+++ switchFlip/neutral.js
@@ -1,1 +1,14 @@
-const neutral = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D, E, F];
+const neutral = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  let neutralColor = "#";
+  for (let i = 0; i < 6; i++) {
+    neutralColor = neutral[0];
+  }
+
+  color.textContent = neutralColor;
+  document.body.style.backgroundColor = neutralColor;
+});
+
