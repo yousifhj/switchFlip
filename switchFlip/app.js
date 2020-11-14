3dea527be6279efc40c385d0c53ef16df8bed1ad
Huda Yousif <hjoski@gmail.com>
2020-11-14 10:30:59 -0800

    Error in index.html and finalize app.js

--- switchFlip/app.js
+++ switchFlip/app.js
@@ -1,11 +1,17 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["gray", "pink", "rgba(133,122,200)", "#f15025"];
 
 const btn = document.getElementById('btn');
 const color = document.querySelector(".color");
 
-btn.addEventListener('click', function () {
+btn.addEventListener("click", function () {
+    const randomNumber = getRandomNumber();
 
-    const randomNumber = 2;
+    console.log(randomNumber);
+
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
-})
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
