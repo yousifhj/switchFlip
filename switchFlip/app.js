3395d71dcc9259d45e3a2cd3240f242534f2e2df
Huda Yousif <hjoski@gmail.com>
2020-11-14 11:19:17 -0800

    update app.js

--- switchFlip/app.js
+++ switchFlip/app.js
@@ -6,7 +6,7 @@ const color = document.querySelector(".color");
 btn.addEventListener("click", function () {
     const randomNumber = getRandomNumber();
 
-    console.log(randomNumber);
+   console.log(randomNumber);
 
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
